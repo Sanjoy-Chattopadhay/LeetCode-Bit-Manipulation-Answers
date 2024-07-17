83fa6ddf72b050770619742d625188f2a3b950fa
Sanjoy Chattopadhyay <chatterjeesanjoy347@gmail.com>
2024-07-18 01:17:57 +0530

    Update code.cpp

--- Binary Number with Alternating Bits./code.cpp
+++ Binary Number with Alternating Bits./code.cpp
@@ -1,1 +1,19 @@
+Code
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int org = n;
+        vector<int> arr;
+        while(org){
+            arr.push_back(org & 1);
+            org >>= 1;
+        }
+        bool flag = true;
+        for(int i = 1 ; i < arr.size() ; i++){
+            if((arr[i] ^ arr[i-1]) == 0) flag = false;
+        }
+        return flag;
+        
+    }
+};
 
